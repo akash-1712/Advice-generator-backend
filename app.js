44eb395b110f2d11125613317240e458ab96a086
akash-1712 <akashbansalab01@gmail.com>
2022-06-09 01:39:41 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const adviceRouter = require("./routes/fetch");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Origin, X-Requested-With,Authorization"
+  );
+  next();
+});
+
+app.use(helmet);
+
+app.use("/fetch", adviceRouter);
+
+app.use((req, res, next) => {
+  throw new Error("not a correct URL(use 'domain/fetch/advice' instead)");
+});
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: "Failed", error: error.message });
+});
+
+app.listen(8080);
